44fcedd6dffc226eeb964ddbbe304802eba38bd9
Livsy90 <artmir90@gmail.com>
2026-05-06 23:38:17 +0400

    Initial Commit

---
+++ ReactiveVsAsync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReactiveVsAsync
+//
+//  Created by Artem Mir on 06.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReactiveVsAsync/ReactiveVsAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveVsAsyncApp.swift
+//  ReactiveVsAsync
+//
+//  Created by Artem Mir on 06.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveVsAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
